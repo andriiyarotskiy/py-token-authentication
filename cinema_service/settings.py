f876ee0eb68d26a18ab636a557849de373bb0d59
Andrii Yarotskiy <andriiyarotskiy@gmail,com>
2026-02-19 19:35:39 +0000

    token authentication

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -127,6 +127,11 @@ USE_I18N = True
 
 USE_TZ = False
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
