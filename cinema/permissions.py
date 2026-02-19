54bdb562ede7422beaa0c52855050729d80c9e75
Andrii Yarotskiy <andriiyarotskiy@gmail,com>
2026-02-19 20:27:00 +0000

    fix

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -3,14 +3,13 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if bool(
-                request.method in SAFE_METHODS
-                and request.user
-                and request.user.is_authenticated
-        ):
-            return True
-        if request.user.is_staff and request.method == "DELETE":
-            return True
+        supported_delete = hasattr(view, "destroy")
+        if request.method == "DELETE":
+            if not supported_delete:
+                return True
+            return bool(request.user and request.user.is_staff)
+        if request.method in SAFE_METHODS:
+            return bool(request.user and request.user.is_authenticated)
         if request.user and request.user.is_staff:
             return True
         return False
